c87c2978ba7273dfbb60e3173302cb3e446b0585
summer00072 <1573819207@qq.com>
2018-04-07 18:21:00 +0800

    git误操作不清楚有啥影响

--- main/main.js
+++ main/main.js
@@ -1,5 +1,17 @@
 // Write your cade below:
-function main() {};
+function main(sysIn,userIn) {
+	let count = [0,0];
+	for (let i = 0; i < sysIn.length; i++) {
+		let temp = userIn.indexOf(sysIn.charAt(i));
+		if (temp != -1 && temp != i) {
+			count[1] += 1;
+		}
+		if (temp == i) {
+			count[0] += 1;
+		}
+	}
+	return `${count[0]}A${count[1]}B`;
+};
 
 module.exports = {
     main
